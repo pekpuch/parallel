4d3bf355b91265e07f30525aaf465667e78006b4
Look to la Luna <114422891+pekpuch@users.noreply.github.com>
2024-05-21 00:28:04 +0700

    Update lab5_paral.py

--- lab5/lab5_paral.py
+++ lab5/lab5_paral.py
@@ -5,119 +5,103 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 from ultralytics import YOLO
 import os
+import math
+import threading
+import queue
+import torch
 
-# Класс-оболочка для управления ресурсом модели
 class ModelResource:
     def __init__(self):
-        # Инициализация ресурса (загрузка модели)
-        print("Loading model...")
         self.model = YOLO('yolov8s-pose.pt')  
-
     def __del__(self):
-        # Освобождение ресурса (выгрузка модели)
-        print("Unloading model...")
         del self.model
 
-# Функция для обработки одного кадра
-def process_frame(frame, model_resource):
-    # Извлечение модели из оболочки-ресурса
+def batch_work (batch, model_resource, q):
     model = model_resource.model
-    # Инференс модели и наложение KeyPoints на кадр
-    results = model.predict(frame, verbose=False)  # Выполнение инференса
-    keypoints = results[0].keypoints  # Извлечение KeyPoints
-    frame_with_keypoints = results[0].plot()  # Наложение KeyPoints на кадр
-    return frame_with_keypoints
+    model.to(torch.device('cpu'))
+    for frame in batch:
+        results = model.predict(frame, verbose=False)
+        keypoints = results[0].keypoints
+        frame_with_keypoints = results[0].plot()
+        q.put(frame_with_keypoints)
+    
 
-# Функция для обработки видео в однопоточном режиме
-def process_video_single_threaded(video_path, output_video_path):
-    # Измерение времени
-    start_time = time.time()
-
-    # Чтение видео
+def video_pred (video_path, output_path, num_threads):
     video = cv2.VideoCapture(video_path)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = 12.88
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) #21
+    batch_size = math.ceil(num_frames / num_threads) #6
+    frame_size = (640, 480)
 
-    # Инициализация оболочки-ресурса модели
-    model_resource = ModelResource()
+    # Разбиение кадров видео на батчи
+    batch = []
+    batches_list =[]
+    i = 0 
+    while (i < num_frames):
+        for j in range (batch_size):
+            if (i == num_frames):
+                break
+            ret, frame = video.read()
+            frame = cv2.resize(frame, (640, 480))
+            batch.append(frame)
+            i+=1
+        batches_list.append(batch)
+        batch = []
+        
+    # Создание очередей и потоков
+    threads_list = []
+    queue_list = []
+    model_list = []
+    thread_idx = 0
+    for batch in batches_list:
+        q = queue.Queue(len(batch))
+        queue_list.append(q)
+        model_resource = ModelResource()
+        model_list.append(model_resource)
+        thread = threading.Thread(target=batch_work, args=(batch, model_list[thread_idx], queue_list[thread_idx]))
+        thread_idx+=1
+        threads_list.append(thread)
 
-    # Обработка каждого кадра
-    for i in range(frame_count):
-        ret, frame = video.read()
-        if not ret:
-            break
-        # Изменение разрешения
-        frame = cv2.resize(frame, (640, 480))
-        processed_frame = process_frame(frame, model_resource)
-
-        # Запись обработанного кадра в выходное видео
-        if i == 0:
-            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-            out_video = cv2.VideoWriter(output_video_path, fourcc, fps, (640, 480))
-        out_video.write(processed_frame)
-
-    # Освобождение ресурсов
-    out_video.release()
-    video.release()
-
-    # Измерение времени
-    end_time = time.time()
-    print(f"Single-threaded processing time: {end_time - start_time:.2f} seconds")
-
-# Функция для обработки видео в многопоточном режиме
-def process_video_multi_threaded(video_path, output_video_path, num_threads):
-    # Измерение времени
+    # Замер времени
     start_time = time.time()
+    
+    # Запуск потоков
+    for thread in threads_list:
+        thread.start()
 
-    # Чтение видео
-    video = cv2.VideoCapture(video_path)
-    fps = video.get(cv2.CAP_PROP_FPS)
-    frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    # Остановка потоков
+    for thread in threads_list:
+        thread.join()
+        
+    # Окончание замера времени
+    total_time = time.time() - start_time
 
-    # Инициализация оболочки-ресурса модели
-    model_resource = ModelResource()
+    # Объединение очередей
+    frames_list = []
+    for q in queue_list:
+        while not q.empty():
+            frames_list.append(q.get())
 
-    # Чтение кадров из видео и создание списка фреймов
-    frames = []
-    for i in range(frame_count):
-        ret, frame = video.read()
-        if not ret:
-            break
-        frames.append(frame)
-
-    # Инициализация пула потоков
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        # Обработка каждого кадра в отдельном потоке
-        future_frames = [executor.submit(process_frame, cv2.resize(frame, (640, 480)), model_resource) for frame in frames]
-
-        # Запись обработанного кадра в выходное видео
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        out_video = cv2.VideoWriter(output_video_path, fourcc, fps, (640, 480))
-        for i, future_frame in enumerate(future_frames):
-            processed_frame = future_frame.result()
-            out_video.write(processed_frame)
-
-    # Освобождение ресурсов
-    out_video.release()
-    video.release()
-
-    # Измерение времени
-    end_time = time.time()
-    print(f"Multi-threaded processing time (with {num_threads} threads): {end_time - start_time:.2f} seconds")
-
-parser = argparse.ArgumentParser(description="-i - имя видео, -m - режим, -o - имя output, -t - ")
-parser.add_argument("-i", help="путь")
-parser.add_argument("-m", choices=["single-threaded", "multi-threaded"], help="режим")
-parser.add_argument("-o", help="путь output")
-parser.add_argument("-t", type=int, default=None, help="число потоков")
-args = parser.parse_args()
-
-# Выбор режима обработки видео
-if args.m == "single-threaded":
-    process_video_single_threaded(args.i, args.o)
-elif args.m == "multi-threaded":
-    if args.t is None:
-        num_threads = os.cpu_count() 
-    else:
-        num_threads = args.t
-    process_video_multi_threaded(args.i, args.o, num_threads)
+    # Создание видео
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
+    out = cv2.VideoWriter(output_path, fourcc, fps, frame_size)  
+    for frame in frames_list: 
+        out.write(frame)
+    out.release()
+    return total_time
+    
+def main (args):
+    video_path = args.input
+    output_path = args.output
+    num_threads = args.num_threads
+    
+    total_time = video_pred (video_path, output_path, num_threads)
+    
+    print(f"для {num_threads} потоков T = {total_time:.4f}")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default='video.mp4', help='Путь до видео (по умолчанию: video.mp4)')
+    parser.add_argument('-t', '--num_threads', type=int, default=1, help='Число потоков (по умолчанию: 1)')
+    parser.add_argument('-o', '--output', type=str, default='output.mp4', help='Путь до обработанного видео (по умолчанию: output.mp4)')
+    args = parser.parse_args()
+    main(args)
